b6d648b68160f3242170c021cfebcf3a99b200d2
BanaNas <106548867+AchylsLuna@users.noreply.github.com>
2024-09-20 18:56:57 +0800

    Add files via upload

---
+++ Fokus-main/app/src/main/java/com/example/fokus/LoginActivity.kt
@@ -0,0 +1,25 @@
+package com.example.fokus
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_login)
+
+        val signupD = findViewById<TextView>(R.id.signupD)
+
+        signupD.setOnClickListener{
+            val Intent = Intent(this, SignupActivity::class.java)
+            startActivity(Intent)
+        }
+
+        }
+    }
