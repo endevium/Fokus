e5a143e797cb9e91bac2422aee25c9ef9a9e5c6d
BanaNas <106548867+AchylsLuna@users.noreply.github.com>
2024-09-20 18:54:52 +0800

    Add files via upload

---
+++ database/migrations/2024_09_16_011649_create_fokus_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('fokus_app', function (Blueprint $table) {
+            $table->id();
+            $table->string('username')->unique();
+            $table->string('fullname');
+            $table->string('password');
+            $table->string('email')->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('fokus_app');
+    }
+};
